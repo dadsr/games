d1aa375388e66d0ddd976f822b6a26bff44a82a1
speedy <modoron46@gmain.com>
2025-03-03 22:10:52 +0200

    commit

--- src/Components/memoryGame/MemoryGame.tsx
+++ src/Components/memoryGame/MemoryGame.tsx
@@ -1,15 +1,38 @@
 import "./MemoryGame.css";
 import shuffle from "../../utils/Shuffles.ts";
 import {MemoryCard} from "./MemoryCard/MemoryCard.tsx";
+import {useState} from "react";
+
+const items =[1,2,3,4,5,6,7,8,9,10];
+const defaultState = { index: -1, value: null };
+const maxColumns:number = 5;
 
 export function MemoryGame(): React.JSX.Element {
-    const items =[1,2,3,4,5,6,7,8,9,10];
-    const allItems:any[] =shuffle([...items,...items]);
-    const columns = Math.min(5, Math.ceil(Math.sqrt(allItems.length))); // Max 5 columns, adjust based on total items
+    const columns = Math.min(maxColumns, Math.ceil(Math.sqrt(allItems.length)));
 
-    function handleCardClick(index: number): void {
+    const allItems:any[] =shuffle([...items,...items]);
+
+    const [firstCard, setFirstCard] = useState(defaultState);
+    const [secondCard, setSecondCard] = useState(defaultState);
+    const remainingCouples:number = items.length;
+    const [moves, setMoves] = useState(0);
+
+    const handleCardClick = (index: number, item)=> {
         console.log(`Card at index ${index} clicked!`);
-        // Add your card flip or game logic here
+        if(firstCard.index === null){
+            setFirstCard({index, item});
+        } else if(firstCard.index !== null && secondCard.index === null){
+            setSecondCard({index, item});
+            setMoves((moves) => moves+1);
+
+        }
+
+        if (firstCard.value === secondCard.value){
+
+        }
+        setFirstCard(defaultState);
+        setSecondCard(defaultState);
+
     }
 
     return(
@@ -21,7 +44,7 @@ export function MemoryGame(): React.JSX.Element {
                 <MemoryCard
                     key = {index}
                     data = {item}
-                    onClick = {() => handleCardClick(index)}
+                    onClick = {() => handleCardClick(index,item)}
                 />
             ))}
         </div>
